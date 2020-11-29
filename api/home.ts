21b1841581a4b15e8741c77892366931cad1fa43
Tom Spencer <tom.spencer@studiozeffa.com>
2020-11-29 18:28:13 +0000

    Initial commit

---
+++ api/home.ts
@@ -0,0 +1,16 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+
+const subdomainParser = /^https:\/\/([a-zA-Z]+)\.threadsforslack\.com\/?$/
+
+export default (req: NowRequest, res: NowResponse) => {
+  let comparative = 'better';
+
+  const host = req.headers['host'];
+  const regexResult = subdomainParser.exec(host);
+  if(regexResult && regexResult[1]) {
+    comparative = regexResult[1];
+  }
+
+  const comparativeCapitalized = comparative.charAt(0).toUpperCase + comparative.slice(1);
+  res.status(200).send(`${comparativeCapitalized}!`)
+}
